6d953690d58c550da68cee734ba884b8ab710e8a
ramialmoh12 <ramiattieh120@gmail.com>
2026-06-12 18:41:54 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/My-Online-Resume1/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
